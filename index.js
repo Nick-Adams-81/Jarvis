6d3eec30fd71326c36eabbe38b076647ca166b15
Nicholas Adams <nicholasadams@Owners-MBP.attlocal.net>
2023-12-13 13:50:15 -0800

    completed jarvis

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-import { OpenAI } from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-});
+import openai from "./config/open-ai.js";
+import readlineSync from "readline-sync";
+import colors from "colors";
 
 const main = async () => {
-    const chatCompletion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "user", content: "what is the capitol of california?" }
-        ]
-    })
-    console.log(chatCompletion.choices[0].message.content)
+    console.log(colors.bold.green("Hello Sir, I am Jarvis, Your personal AI assistant"));
+    console.log(colors.bold.green("How may I help you today?"));
+
+    const chatHistory  = [];
+
+    while(true) {
+        const userInput = readlineSync.question(colors.blue("You: "));
+        try {
+            const messages = chatHistory.map(([role, content]) => ({ role, content }));
+            messages.push({ role: "user", content: userInput })
+            const chatCompletion = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: messages,
+            })
+            const completionText = chatCompletion.choices[0].message.content;
+            if(userInput.toLowerCase() === "exit" || userInput.toLowerCase() === "quit") {
+                console.log(colors.green("JARVIS: ") + completionText);
+                return;
+            }
+            console.log(colors.green("JARVIS: ") + completionText);
+            chatHistory.push(["user", userInput]);
+            chatHistory.push(["assistant", completionText]);
+        } catch(err) {
+            console.error(colors.red(error));
+        }
+    }
+    
 }
 
 main();
