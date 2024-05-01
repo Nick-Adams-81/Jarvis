435c7527ff2370329b636b3ce579f0e8fe4049c1
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-04-30 19:53:21 -0700

    update

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import readlineSync from "readline-sync";
 import colors from "colors";
 
 const main = async () => {
-    console.log(colors.bold.green("Hello Sir, I am Jarvis, Your personal AI assistant"));
-    console.log(colors.bold.green("How may I help you today?"));
+    console.log(colors.bold.brightYellow(colors.brightMagenta("Jarvis: ") + "Hello Sir, I am Jarvis, Your personal AI assistant"));
+    console.log(colors.bold.brightYellow(colors.brightMagenta("Jarvis: ") + "How may I help you today?"));
 
     const chatHistory  = [];
 
     while(true) {
-        const userInput = readlineSync.question(colors.blue("You: "));
+        const userInput = readlineSync.question(colors.bold.brightCyan("You: "));
         try {
             const messages = chatHistory.map(([role, content]) => ({ role, content }));
             messages.push({ role: "user", content: userInput })
@@ -19,14 +19,14 @@ const main = async () => {
             })
             const completionText = chatCompletion.choices[0].message.content;
             if(userInput.toLowerCase() === "exit" || userInput.toLowerCase() === "quit") {
-                console.log(colors.green("JARVIS: ") + completionText);
+                console.log(colors.bold.brightMagenta("JARVIS: ") + colors.bold.brightMagenta(completionText));
                 return;
             }
-            console.log(colors.green("JARVIS: ") + completionText);
+            console.log(colors.bold.brightMagenta("JARVIS: ") + colors.bold.brightYellow(completionText));
             chatHistory.push(["user", userInput]);
             chatHistory.push(["assistant", completionText]);
         } catch(err) {
-            console.error(colors.red(error));
+            console.error(colors.red(err));
         }
     }
     
